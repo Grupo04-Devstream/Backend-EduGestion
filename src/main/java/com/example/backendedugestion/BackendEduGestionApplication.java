e2f3a66c0a7b74bfe68631be118b82e8f2841f4f
Jmond544 <jmondalgotapia@gmail.com>
2024-03-06 13:37:30 -0500

    First commit project

---
+++ src/main/java/com/example/backendedugestion/BackendEduGestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendedugestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEduGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEduGestionApplication.class, args);
+	}
+
+}
